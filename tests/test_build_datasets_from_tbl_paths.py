c1f58ec9d6cdc5be810acb7317c1ccb28e13670c
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-20 18:57:19 +0000

    copy test_build_datasets_from_tbl.py to test_build_datasets_from_tbl_paths.py

---
+++ tests/test_build_datasets_from_tbl_paths.py
@@ -0,0 +1,52 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+import os
+import sys
+import collections
+
+import pytest
+
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
+
+import atnanoaod
+
+##__________________________________________________________________||
+@pytest.fixture()
+def tbl_cmsdatasets():
+    thisdir = os.path.dirname(os.path.abspath(__file__))
+    tblpath = os.path.join(thisdir, 'tbl', 'tbl_dataset_cmsdataset_01.txt')
+    return tblpath
+
+@pytest.fixture()
+def mock_build_datasets(monkeypatch):
+    ret = mock.Mock()
+    module = sys.modules['atnanoaod.query']
+    monkeypatch.setattr(module, 'build_datasets', ret)
+    return ret
+
+def test_build_datasets_from_tbl(tbl_cmsdatasets, mock_build_datasets):
+
+    atnanoaod.query.build_datasets_from_tbl(
+        tbl_dataset_cmsdataset=tbl_cmsdatasets
+    )
+
+    expected = [mock.call(collections.OrderedDict([
+        ('QCD_HT200to300', ['/QCD_HT200to300_13TeV/05Feb2018-v1/NANOAODSIM']),
+        ('QCD_HT500to700', [
+            '/QCD_HT500to700_13TeV/05Feb2018-v1/NANOAODSIM',
+            '/QCD_HT500to700_13TeV/05Feb2018_ext1-v1/NANOAODSIM']),
+        ('QCD_HT700to1000', ['/QCD_HT700to1000_13TeV/05Feb2018_ext1-v1/NANOAODSIM']),
+        ('QCD_HT1000to1500', ['/QCD_HT1000to1500_13TeV/05Feb2018-v1/NANOAODSIM']),
+        ('QCD_HT1500to2000', ['/QCD_HT1500to2000_13TeV/05Feb2018_ext1-v1/NANOAODSIM']),
+        ('QCD_HT2000toInf', [
+            '/QCD_HT2000toInf_13TeV/05Feb2018-v1/NANOAODSIM',
+            '/QCD_HT2000toInf_13TeV/05Feb2018_ext1-v1/NANOAODSIM'
+        ])
+    ]))]
+
+    print mock_build_datasets.call_args_list
+    assert expected == mock_build_datasets.call_args_list
+
+##__________________________________________________________________||
