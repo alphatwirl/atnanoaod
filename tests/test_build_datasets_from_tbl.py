5f0646c5fe440a561b96a45c4719e0184fb308f3
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-20 18:57:04 +0000

    update test_build_datasets_from_tbl()

--- tests/test_build_datasets_from_tbl.py
+++ tests/test_build_datasets_from_tbl.py
@@ -1,6 +1,8 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
 import os
 import sys
+import collections
+
 import pytest
 
 try:
@@ -18,46 +20,33 @@ def tbl_cmsdatasets():
     return tblpath
 
 @pytest.fixture()
-def mock_channel():
-    ret = mock.Mock()
-    ret.results = [ ]
-    ret.put.side_effect = lambda t, *a, **k: ret.results.append(t(*a, **k))
-    ret.receive.return_value = ret.results
-    return ret
-
-def test_mock_channel(mock_channel):
-    mock_channel.put(pow, 2, 3)
-    mock_channel.put(dict, a=10, b=30)
-    assert [8, {'a': 10, 'b': 30}] == mock_channel.receive()
-
-@pytest.fixture()
-def mock_alphatwirl(monkeypatch, mock_channel):
+def mock_build_datasets(monkeypatch):
     ret = mock.Mock()
     module = sys.modules['atnanoaod.query']
-    monkeypatch.setattr(module, 'alphatwirl', ret)
-    ret.parallel.build_parallel().communicationChannel = mock_channel
+    monkeypatch.setattr(module, 'build_datasets', ret)
     return ret
 
-@pytest.fixture()
-def mock_mk_dataset_files_list(monkeypatch, mock_alphatwirl):
-    ret = mock.Mock()
-    module = sys.modules['atnanoaod.query']
-    monkeypatch.setattr(module, 'mk_dataset_files_list', ret)
-    return ret
-
-def test_build_datasets_from_tbl(tbl_cmsdatasets, mock_mk_dataset_files_list):
+def test_build_datasets_from_tbl(tbl_cmsdatasets, mock_build_datasets):
 
     atnanoaod.query.build_datasets_from_tbl(
         tbl_dataset_cmsdataset=tbl_cmsdatasets
     )
-    expected = [
-        mock.call('QCD_HT200to300', ['/QCD_HT200to300_13TeV/05Feb2018-v1/NANOAODSIM']),
-        mock.call('QCD_HT500to700', ['/QCD_HT500to700_13TeV/05Feb2018-v1/NANOAODSIM', '/QCD_HT500to700_13TeV/05Feb2018_ext1-v1/NANOAODSIM']),
-        mock.call('QCD_HT700to1000', ['/QCD_HT700to1000_13TeV/05Feb2018_ext1-v1/NANOAODSIM']),
-        mock.call('QCD_HT1000to1500', ['/QCD_HT1000to1500_13TeV/05Feb2018-v1/NANOAODSIM']),
-        mock.call('QCD_HT1500to2000', ['/QCD_HT1500to2000_13TeV/05Feb2018_ext1-v1/NANOAODSIM']),
-        mock.call('QCD_HT2000toInf', ['/QCD_HT2000toInf_13TeV/05Feb2018-v1/NANOAODSIM', '/QCD_HT2000toInf_13TeV/05Feb2018_ext1-v1/NANOAODSIM'])
-    ]
-    assert expected == mock_mk_dataset_files_list.call_args_list
+
+    expected = [mock.call(collections.OrderedDict([
+        ('QCD_HT200to300', ['/QCD_HT200to300_13TeV/05Feb2018-v1/NANOAODSIM']),
+        ('QCD_HT500to700', [
+            '/QCD_HT500to700_13TeV/05Feb2018-v1/NANOAODSIM',
+            '/QCD_HT500to700_13TeV/05Feb2018_ext1-v1/NANOAODSIM']),
+        ('QCD_HT700to1000', ['/QCD_HT700to1000_13TeV/05Feb2018_ext1-v1/NANOAODSIM']),
+        ('QCD_HT1000to1500', ['/QCD_HT1000to1500_13TeV/05Feb2018-v1/NANOAODSIM']),
+        ('QCD_HT1500to2000', ['/QCD_HT1500to2000_13TeV/05Feb2018_ext1-v1/NANOAODSIM']),
+        ('QCD_HT2000toInf', [
+            '/QCD_HT2000toInf_13TeV/05Feb2018-v1/NANOAODSIM',
+            '/QCD_HT2000toInf_13TeV/05Feb2018_ext1-v1/NANOAODSIM'
+        ])
+    ]))]
+
+    print mock_build_datasets.call_args_list
+    assert expected == mock_build_datasets.call_args_list
 
 ##__________________________________________________________________||
