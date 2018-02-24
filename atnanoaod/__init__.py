82e9d5b3a73ad90e2a3f2ffcf03c65e2e2096c3f
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-24 18:11:54 +0000

    add eventbuilder.py

--- atnanoaod/__init__.py
+++ atnanoaod/__init__.py
@@ -3,6 +3,7 @@ from .fw import AtNanoAOD
 from . import dasquery
 from . import query
 from . import dataset
+from . import eventbuilder
 
 from ._version import get_versions
 __version__ = get_versions()['version']
---
+++ atnanoaod/eventbuilder.py
@@ -0,0 +1,29 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+import ROOT
+
+from alphatwirl.roottree import BEvents
+
+from .cmsfilepath import convert_lfn_to_pfn_or_aaa
+
+##__________________________________________________________________||
+class EventBuilder(object):
+    def __init__(self, config):
+        self.config = config
+
+    def __repr__(self):
+        return '{}({!r})'.format(
+            self.__class__.__name__,
+            self.config
+        )
+
+    def __call__(self):
+        chain = ROOT.TChain(self.config.treeName)
+        for path in self.config.inputPaths:
+            path = convert_lfn_to_pfn_or_aaa(path)
+            print path
+            chain.Add(path)
+        events = BEvents(chain, self.config.maxEvents, self.config.start)
+        events.config = self.config
+        return events
+
+##__________________________________________________________________||
